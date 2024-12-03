e4232da7b19229a50b8f8c29f896876bdf86d61c
xbonda06 <mediabondar@gmail.com>
2024-12-03 19:06:16 +0100

    feat: add wifi connection

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 #include "freertos/task.h"
 #include "driver/ledc.h"
 #include "esp_err.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
 
 #define BUZZER_PIN 25
 
@@ -12,7 +16,42 @@
 #define LEDC_DUTY_RES     LEDC_TIMER_8_BIT
 #define LEDC_FREQUENCY    440
 
+static const char *TAG = "WiFi";
+
+#define WIFI_SSID "Pixel"
+#define WIFI_PASS "12131415"
+void wifi_init() {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "Connecting to Wi-Fi...\n");
+    ESP_ERROR_CHECK(esp_wifi_connect());
+}
+
 void app_main(void) {
+    wifi_init();
+
     ledc_timer_config_t ledc_timer = {
         .duty_resolution = LEDC_DUTY_RES,
         .freq_hz = LEDC_FREQUENCY,
