1a54359d94efc4e8d9cdbf349c531fd31bfd6179
xbonda06 <xbonda06@vutbr.cz>
2024-12-05 19:41:53 +0100

    fix: wifi with password connection

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@
 #define LEDC_FREQUENCY    440
 
 #define WIFI_SSID "Pixel"
-#define WIFI_PASS ""
+#define WIFI_PASS "12131415"
 
 static const char *TAG = "WiFi";
 
@@ -43,11 +43,11 @@ static int calculate_sound_time(){
 
 static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        printf("Wi-Fi: STA started\n");
+        printf("WIFI CONNECTING...\n");
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_CONNECTED) {
-        printf("Wi-Fi: Connected\n");
+        printf("WIFI CONNECTED\n");
     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        printf("Wi-Fi: Disconnected, reconnecting...\n");
+        printf("WiFi lost connection\n");
         esp_wifi_connect();
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
@@ -67,8 +67,11 @@ void wifi_init() {
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
 
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&wifi_initiation));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL));
 
     wifi_config_t wifi_config = {
         .sta = {
@@ -77,12 +80,9 @@ void wifi_init() {
         },
     };
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 
     printf("Connecting to Wi-Fi...\n");
     esp_wifi_connect();
